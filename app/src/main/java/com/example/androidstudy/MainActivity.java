8393f7879d6e1cd0641c082a75c955af689e7ac9
YunSongHui <dbsthdgml123@naver.com>
2020-02-06 19:47:05 +0900

    CH2.3/버튼 클릭 시 Toast 메세지 출력/20200206

--- app/src/main/java/com/example/androidstudy/MainActivity.java
+++ app/src/main/java/com/example/androidstudy/MainActivity.java
@@ -1,15 +1,29 @@
 package com.example.androidstudy;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "클릭되었습니다", Toast.LENGTH_SHORT).show();
+            }
+        });
+
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setIcon(R.drawable.android_icon);
     }
